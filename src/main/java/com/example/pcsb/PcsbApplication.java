d85c58b621bb52745ff63266f735f36a50398253
samuelblazquezdiaz <sblazquez2002@gmail.com>
2024-09-22 10:58:32 +0200

    Initial Commit

---
+++ src/main/java/com/example/pcsb/PcsbApplication.java
@@ -0,0 +1,13 @@
+package com.example.pcsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcsbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcsbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pcsb/PcsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pcsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcsbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
